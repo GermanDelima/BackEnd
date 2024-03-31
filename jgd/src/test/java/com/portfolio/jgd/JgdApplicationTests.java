0c4ef6aa8b647417c092ecacb137a289942f08b9
Josias <josiasdelima080@gmail.com>
2024-03-30 21:38:50 -0300

    initt backend

---
+++ jgd/src/test/java/com/portfolio/jgd/JgdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.jgd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
